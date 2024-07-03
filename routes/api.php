9bf5b9df4f201e73e4b2919b5db8cc51c2bf7830
itoktoni <itok.toni@gmail.com>
2024-07-03 13:00:24 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -661,7 +661,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
 
         $data = ViewTotalJenis::where(ViewTotalJenis::field_rs_id(), $rsid)
             ->where(ViewTotalJenis::field_primary(), $jenis)
-            ->first() ?? [];
+            ->first() ?? false;
 
         return Notes::data($data);
     });
