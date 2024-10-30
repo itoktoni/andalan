89eea9dcc977322caf9093c74c62cff7896fc162
itoktoni <itok.toni@gmail.com>
2024-10-30 17:43:51 +0700

    update berat

--- resources/views/pages/jenis_linen/form.blade.php
+++ resources/views/pages/jenis_linen/form.blade.php
@@ -7,6 +7,7 @@
 
             <x-form-select col="6" class="search" name="jenis_id_kategori" :options="$category" />
             <x-form-input col="6" name="jenis_nama" label="Nama Linen"/>
+            <x-form-input col="6" name="jenis_berat" label="Berat"/>
             <x-form-textarea col="6" rows="4" name="jenis_deskripsi" />
 
             @endbind
