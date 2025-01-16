6d6fafe659816cd08b4a4fc3e7c548b8785ce278
itoktoni <itok.toni@gmail.com>
2025-01-16 20:16:50 +0700

    update dashboard

--- app/Charts/DashboardBersihHarian.php
+++ app/Charts/DashboardBersihHarian.php
@@ -36,10 +36,20 @@ class DashboardBersihHarian
             ->whereDate(Transaksi::field_created_at(), '<=', $end_date->format('Y-m-d'))
             ->where(Transaksi::field_status_transaction(), TransactionType::KOTOR);
 
+        $data_rewash = Transaksi::whereDate(Transaksi::field_created_at(), '>=', $start_date->format('Y-m-d'))
+            ->whereDate(Transaksi::field_created_at(), '<=', $end_date->format('Y-m-d'))
+            ->where(Transaksi::field_status_transaction(), TransactionType::REWASH);
+
+        $data_reject = Transaksi::whereDate(Transaksi::field_created_at(), '>=', $start_date->format('Y-m-d'))
+            ->whereDate(Transaksi::field_created_at(), '<=', $end_date->format('Y-m-d'))
+            ->where(Transaksi::field_status_transaction(), TransactionType::REJECT);
+
         if ($rs_id) {
 
             $data_bersih = $data_bersih->where(Bersih::field_rs_id(), auth()->user()->rs_id);
             $data_kotor = $data_kotor->where(Transaksi::field_rs_ori(), auth()->user()->rs_id);
+            $data_rewash = $data_rewash->where(Transaksi::field_rs_ori(), auth()->user()->rs_id);
+            $data_reject = $data_reject->where(Transaksi::field_rs_ori(), auth()->user()->rs_id);
         }
 
         $data_bersih = $data_bersih->get();
@@ -48,17 +58,31 @@ class DashboardBersihHarian
             return $item;
         });
 
+        $data_reject = $data_reject->get()->map(function ($item) {
+            $item['tanggal'] = $item->transaksi_created_at->format('Y-m-d') ?? null;
+            return $item;
+        });
+
+        $data_rewash = $data_rewash->get()->map(function ($item) {
+            $item['tanggal'] = $item->transaksi_created_at->format('Y-m-d') ?? null;
+            return $item;
+        });
+
         foreach ($range as $dates) {
             $date[] = $dates->format('m-d');
             $bersih[] = $data_bersih->where(Bersih::field_report(), $dates->format('Y-m-d'))->count();
             $kotor[] = $data_kotor->where('tanggal', $dates->format('Y-m-d'))->count();
+            $reject[] = $data_reject->where('tanggal', $dates->format('Y-m-d'))->count();
+            $rewash[] = $data_rewash->where('tanggal', $dates->format('Y-m-d'))->count();
         }
 
         return $this->chart->barChart()
-            ->setTitle('Perbandingan Bersih dan Kotor 7 hari kebelakang')
-            ->setSubtitle('Bersih vs Kotor.')
+            ->setTitle('Perbandingan Data Transaksi 7 hari kebelakang')
+            ->setSubtitle('Bersih vs Kotor vs Rewash vs Reject.')
             ->addData('Bersih', $bersih)
             ->addData('Kotor', $kotor)
+            ->addData('Rewash', $rewash)
+            ->addData('Reject', $reject)
             ->setXAxis($date);
     }
 }
