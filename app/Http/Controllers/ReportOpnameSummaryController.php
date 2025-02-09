12e86892d81b803436be9db33c4855bdfe59eebd
itoktoni <itok.toni@gmail.com>
2025-02-09 22:29:54 +0700

    update opname summary

--- app/Http/Controllers/ReportOpnameSummaryController.php
+++ app/Http/Controllers/ReportOpnameSummaryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Dao\Models\Detail;
 use App\Dao\Models\Opname;
+use App\Dao\Models\OpnameDetail;
 use App\Dao\Repositories\OpnameRepository;
 use Illuminate\Http\Request;
 use Plugins\Query;
@@ -38,7 +39,7 @@ class ReportOpnameSummaryController extends MinimalController
         $this->data = $this->getQuery($request->opname_id)->get();
 
         $opname = Opname::with(['has_rs'])->find(request()->get(Opname::field_primary()));
-        $register = Detail::where(Detail::field_rs_id(), $opname->field_rs_id)->count();
+        $register = OpnameDetail::where(OpnameDetail::field_rs_id(), $opname->field_rs_id)->count();
 
         return moduleView(modulePathPrint(), $this->share([
             'data' => $this->data,
