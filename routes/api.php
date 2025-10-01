29f7a1fe2ea2ffe3e8135a8cac4a1a131002c7da
itok toni <itok.toni@gmail.com>
2025-10-01 14:49:31 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -708,7 +708,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
                             Outstanding::field_key() => $autoNumber,
                             Outstanding::field_status_transaction() => TransactionType::REGISTER,
                             Outstanding::field_created_at() => $detail->detail_created_at->format('Y-m-d H:i:s') ?? $date,
-                            Outstanding::field_created_by() => $user->detail_updated_at->format('Y-m-d H:i:s') ?? $user,
+                            Outstanding::field_created_by() => $detail->detail_updated_at->format('Y-m-d H:i:s') ?? $user,
                         ]));
                     }
 
