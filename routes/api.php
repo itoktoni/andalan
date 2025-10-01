00e5b1b6f1d3a668884f56bd7018059f63591b54
itok toni <itok.toni@gmail.com>
2025-10-01 14:45:49 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -702,13 +702,13 @@ Route::middleware(['auth:sanctum'])->group(function () {
                 }
                 else
                 {
-                    if($detail->field_status_linen == TransactionType::REGISTER)
+                    if($detail->field_status_linen == TransactionType::REGISTER || $detail->field_status_linen == TransactionType::REGISTER)
                     {
                         $outstanding = Outstanding::create(array_merge($data_outstanding, [
                             Outstanding::field_key() => $autoNumber,
                             Outstanding::field_status_transaction() => TransactionType::REGISTER,
-                            Outstanding::field_created_at() => $detail->detail_created_at,
-                            Outstanding::field_created_by() => $user->detail_updated_at,
+                            Outstanding::field_created_at() => $detail->detail_created_at->format('Y-m-d H:i:s') ?? $date,
+                            Outstanding::field_created_by() => $user->detail_updated_at->format('Y-m-d H:i:s') ?? $user,
                         ]));
                     }
 
